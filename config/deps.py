00c78ad402f7f6709346e0fbc527f83af0219406
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-25 21:30:00 +0300

    fixing deps

--- config/deps.py
+++ config/deps.py
@@ -1,16 +1,6 @@
-'''
-dependencies for this project
-'''
-
-def populate(d):
-    d.packs=[
-        'lilypond',
-        'lilypond-doc',
-        'lilypond-doc-html',
-        'lilypond-doc-pdf',
-    ]
-
-def getdeps():
-    return [
-        __file__, # myself
-    ]
+packages=[
+    'lilypond',
+    'lilypond-doc',
+    'lilypond-doc-html',
+    'lilypond-doc-pdf',
+]
