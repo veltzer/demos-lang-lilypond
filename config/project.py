758f28202ff7e015bf6e2b62294ca24306b48efe
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:56 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-lilypond"
-project_long_description = "Demos for the lilypond language"
-project_year_started = 2014
-project_description = """Demos for the lilypond language"""
+description_short = "Demos for the lilypond language"
